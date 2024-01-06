1f6c2014d99a06aa0fef58c713ffeb0c58c44651
Rajesh Korlapati <77844866+rajesh2150@users.noreply.github.com>
2024-01-06 21:00:23 +0530

    Update script.js with closure def and example

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-
+//Closure :Function bundled with its lexical environment is known as a closure. Whenever function is returned, even if its vanished in execution context but still it remembers the reference it was pointing to.
+  //example
+  function x(){
+  let a=10;
+    function y(){
+      console.log(a)
+    }
+    y()
+  }
+x() //10
+let z=x()
+.... after 1000+lines we can call z() then
+z() //10
